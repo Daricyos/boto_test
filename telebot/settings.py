ffc10bb33d8261feb5e9001d06a1f0f21ba5137c
Daricyos <daricyos@gmail.com>
2021-10-14 00:19:13 +0300

    install dj-database-url

--- telebot/settings.py
+++ telebot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from config import SECRET_KEY_DJANGO, TOKEN_BOT
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,6 +103,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
